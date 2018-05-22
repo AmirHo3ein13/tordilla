7d2371c419c7b7459269d8674125f8481d5511ef
amirho3ein13 <a.bahrami9675@gmail.com>
2018-05-22 15:39:20 +0430

    change the way of reservation

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -19,7 +19,10 @@ class OrdersController extends Controller
     public function add(Request $request){
         $order_details = $request->get('order_details');
 
-        $this->order_detail($order_details);
+        $ret = $this->order_detail($order_details);
+        if (sizeof($ret) and !($request->has('reserve') and $request->get('reserve'))){
+            return json_encode(['status' => false, 'products' => $ret]);
+        }
         return json_encode(
             Order::create([
                 'customer_id' => $request->get('customer_id'),
@@ -35,12 +38,19 @@ class OrdersController extends Controller
                 'voice' => $request->has('voice') ?
                     $request->file('voice')->store('voice') : null,
                 'description' => $request->get('description'),
+                'reserve' => $request->has('reserve') ? $request->get('reserve') : false
             ])
         );
     }
 
+    /**
+     * @param $detail
+     * @return array
+     */
     private function order_detail($detail){
+        $not_enough = array();
         $orders = array_slice(explode('|', $detail), 0, -1);
+        $products = array();
         foreach ($orders as $order){
             $id_number = explode(':', $order);
             $id = $id_number[0];
@@ -49,18 +59,21 @@ class OrdersController extends Controller
             if (strpos($pack, 'e') != false)
                 $pack = substr($pack, 1);
             $product = Product::where('code', $id)->first();
+            array_push($products, $product);
             if (!$product)
                 abort(500);
             $pack += $box_pack[0] * $product['number_in_box'];
             if ($product['inventory'] >= $pack){
                 $product['inventory'] -= $pack;
-                $product->save();
             }
             else{
-                $product['reservation_inventory'] -= $pack;
-                $product->save();
+                array_push($not_enough, $product['id']);
             }
         }
+        if (!sizeof($not_enough))
+            foreach ($products as $product)
+                $product->save();
+        return $not_enough;
     }
 
     /**
@@ -140,16 +153,29 @@ class OrdersController extends Controller
 //        return json_encode(true);
 //    }
 
+    /**
+     * @param $id
+     * @return string
+     */
     public function delete($id){
         Order::destroy($id);
         return json_encode(true);
     }
 
+    /**
+     * @param $id
+     * @param Request $request
+     * @return string
+     */
     public function update($id, Request $request){
         $order = Order::findOrFail($id);
         if (Role::find(Auth::user()['role']) == 'sales manager' and
             strtotime($order['created_at'].' + 1 day') >= time()){
-            abort(404);
+            return json_encode('update date expired');
+        }
+        elseif (Role::find(Auth::user()['role']) == 'sales supervisor' and
+            $order['transmission_day'] != null){
+            return json_encode('update date expired');
         }
         $order->customer_id = $request->get('customer_id');
         $order->marketer_id = $request->get('marketer_id');
@@ -159,6 +185,7 @@ class OrdersController extends Controller
         $order->discount = $request->get('discount');
         $order->latitude = $request->has('latitude') ? $request->get('latitude') : null;
         $order->longitude = $request->has('longitude') ? $request->get('longitude') : null;
+        $order->reserve = $request->has('reserve') ? $request->get('reserve') : $order->reserve;
         $order->image = $request->has('image') ?
             $request->file('image')->store('order_image') : null;
         $order->voice = $request->has('voice') ?
@@ -166,9 +193,11 @@ class OrdersController extends Controller
         $order->description = $request->get('description');
         if ($request->exists('driver')){
             $order->driver = $request->get('driver');
+            $order->transmission_day = $request->has('transmission_day') ?
+                $request->get('transmission_day') : null;
             $order->step = 1;
         }
-        elseif ($request->exists('factor')){
+        if ($request->exists('factor')){
             $order->factor_number = $request->get('factor');
             $order->step = 2;
         }
